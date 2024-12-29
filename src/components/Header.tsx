368fedaef40bf0358cb36c7dbc01a8678d69f127
MilanMareels <milan.mareels@gmail.com>
2024-12-29 18:15:24 +0100

    fixex header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
 import { Carousel } from "flowbite-react";
+import { headerImages } from "../data/headerImages";
 
 export default function Header() {
   return (
     <header>
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel slideInterval={3000}>
-          <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+          {headerImages.map((image) => (
+            <img src={image.image} alt="..." />
+          ))}
         </Carousel>
       </div>
     </header>
